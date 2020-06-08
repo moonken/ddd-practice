3b3b14e0fe0998776085fda749b0593cef80eabb
Chen Liang <>
2020-06-08 11:48:44 +0800

    add code duplicated check

--- domain/src/main/java/com/thoughtworks/dddpractice/domain/goods/GoodsFactory.java
+++ domain/src/main/java/com/thoughtworks/dddpractice/domain/goods/GoodsFactory.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.dddpractice.domain.goods;
 
+import com.thoughtworks.dddpractice.domain.goods.exception.GoodsCodeDuplicatedException;
 import com.thoughtworks.dddpractice.domain.goods.vo.GoodsVO;
 import com.thoughtworks.dddpractice.framework.annotations.domain.DomainFactory;
+import com.thoughtworks.dddpractice.framework.annotations.domain.Invariant;
 import com.thoughtworks.dddpractice.framework.annotations.domain.Invariants;
 import com.thoughtworks.dddpractice.framework.support.domain.DomainEventPublisher;
 import lombok.AllArgsConstructor;
@@ -18,7 +20,12 @@ public class GoodsFactory {
   private final DomainEventPublisher domainEventPublisher;
   private final GoodsRepository goodsRepository;
 
+  @Invariant("duplicates")
   public Goods create(GoodsVO goodsVO) {
+    if (goodsRepository.findByCode(goodsVO.getCode()).isPresent()) {
+      throw new GoodsCodeDuplicatedException(goodsVO.getCode());
+    }
+
     Goods goods = new Goods(UUID.randomUUID().toString(), goodsVO, domainEventPublisher);
     goodsRepository.save(goods);
     domainEventPublisher.publish(new GoodsCreatedEvent(goods.getAggregateId()));
